ab4b705c715c0109c4a242b6f6e3b9dd7eb40516
Przemysław Przebieglec <p.przebieglec@gmail.com>
2019-06-11 14:57:38 +0200

    Save every commit data in file

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 const http = require("http");
 const fs = require("fs");
+const axios = require("axios");
 
 http
   .createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "application/json" });
     req.on("data", chunk => {
-      fs.appendFile(
-        "file.json",
-        chunk,
-        e => {
-          if (e) {
-            throw e;
-          }
-          console.log("Saved!");
-        }
-      );
+      const payload = JSON.parse(chunk);
+      payload.commits.map(item => getCommitDiff(item, payload.repository));
     });
     res.write(req.url);
     res.end("Hello world");
   })
   .listen(8001);
+
+const getCommitDiff = ({ id }, { full_name }) => {
+  axios(`https://api.github.com/repos/${full_name}/commits/${id}`).then(e =>
+    fs.appendFile(`${id}Commit.json`, JSON.stringify(e.data), q => {
+      if (q) throw q;
+      console.log(`Saved commit: ${e.data.commit.message}`);
+    })
+  );
+};
