9c8b0d7dde228b0a19b68b73baa56969d87cee3a
Przemysław Przebieglec <p.przebieglec@gmail.com>
2019-07-03 12:41:01 +0200

    Change addresses accordingly to action performed

--- App.js
+++ App.js
@@ -6,13 +6,18 @@ const getLineNumber = require("./helpers/getLineNumber/getLineNumber");
 const checkIfCodeContainsMixpanel = require("./helpers/checkIfCodeContainsMixpanel/checkIfCodeContainsMixpanel");
 const getWholeRepo = require("./getDataFromRepo/getWholeRepo");
 const getReposContents = require("./getDataFromRepo/getReposContents");
-  http
-    .createServer((req, res) => {
-      res.writeHead(200, { "Content-Type": "application/json" });
-      req.on("data", async chunk => {
-        const payload = JSON.parse(chunk);
-
-        const files = await getReposContents(
+http
+  .createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    req.on("data", async chunk => {
+      const payload = JSON.parse(chunk);
+      let files;
+      if (
+        payload.action === "closed" ||
+        payload.action === "reopened" ||
+        payload.action === "opened"
+      ) {
+        files = await getReposContents(
           payload.repository,
           "",
           payload.pull_request.head.ref
@@ -23,29 +28,40 @@ const getReposContents = require("./getDataFromRepo/getReposContents");
             payload.pull_request.head.ref
           );
         });
-        const cos = await Promise.all(reduceFilesToOneArray(files))
-          .then(e => e)
-          .catch(e => console.log(e));
-        const odp = await Promise.all(
-          cos.map(async e => {
-            const rawFile = await getRawFile(e.download_url);
-            return checkIfCodeContainsMixpanel(rawFile) && e;
+      }
+      if (payload.action === "added") {
+        files = await Promise.all(
+          payload.repositories_added.map(async repository => {
+            return await getReposContents(repository, "").then(async e => {
+              return await getWholeRepo(e, repository);
+            });
           })
-        ).then(e => e.filter(item => item));
-        Promise.all(
-          odp.map(async e => {
-            const rawFile = await getRawFile(e.download_url);
-            return getLineNumber(
-              rawFile.substring(
-                0,
-                rawFile.toUpperCase().indexOf("mixpanel".toUpperCase())
-              ),
-              "\n"
-            );
-          })
-        ).then(e => console.log(e));
-      });
-      res.write(req.url);
-      res.end("Hello world");
-    })
-    .listen(8001);
+        ).then(e => e);
+      }
+      if(!files) return;
+      const cos = await Promise.all(reduceFilesToOneArray(files))
+        .then(e => e)
+        .catch(e => console.log(e));
+      const odp = await Promise.all(
+        cos.map(async e => {
+          const rawFile = await getRawFile(e.download_url);
+          return checkIfCodeContainsMixpanel(rawFile) && e;
+        })
+      ).then(e => e.filter(item => item));
+      Promise.all(
+        odp.map(async e => {
+          const rawFile = await getRawFile(e.download_url);
+          return getLineNumber(
+            rawFile.substring(
+              0,
+              rawFile.toUpperCase().indexOf("mixpanel".toUpperCase())
+            ),
+            "\n"
+          );
+        })
+      ).then(e => console.log(e));
+    });
+    res.write(req.url);
+    res.end("Hello world");
+  })
+  .listen(8001);
